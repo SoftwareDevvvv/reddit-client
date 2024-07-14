e2fd98036f64531fd38971d4554d20aad2033a27
Malek Bejaoui <malek.brj3@gmail.com>
2024-07-15 00:05:15 +0200

    Create store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
+import { configureStore } from "@reduxjs/toolkit";
+import subredditsReducer from "../features/subreddit/subreddits/subredditsSlice";
+import selectedSubredditReducer from "../features/subreddit/subreddits/selectedSubredditSlice";
+import searchTermReducer from "../features/search/searchTermSlice";
+import postsSlice from "../features/subreddit/posts/postsSlice";
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    subreddits: subredditsReducer,
+    selectedSubreddit: selectedSubredditReducer,
+    searchTerm: searchTermReducer,
+    posts: postsSlice,
   },
 });
